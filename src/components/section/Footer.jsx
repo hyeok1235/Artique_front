7fd490f47ce65d1f565d797522ba4645bea06149
kimyerak <yerak213@gmail.com>
2024-11-09 22:46:05 +0900

    yerak_design_fin

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 export default function Footer() {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        textAlign: "center",
+
+        color: "white", // 글씨색 (선택 사항)
+        padding: "10px 0", // 위아래 여백
+      }}
+    >
+      <p>Copyright &copy;inthon_team1</p>
+    </div>
+  );
 }
